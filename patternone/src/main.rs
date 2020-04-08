2209b4bf2848b7f148cbabfa019bf47794c818a1
Davide Prati <lastexxit@gmail.com>
2020-04-08 19:20:08 +0200

    add things

--- patternone/src/main.rs
+++ patternone/src/main.rs
@@ -2,7 +2,8 @@ use nannou::prelude::*;
 mod colors;
 use crate::colors::Palette;
 use nannou::color::gradient::Gradient;
-use nannou::geom::graph::edge::Axis::Y;
+use nannou::rand::rngs::StdRng;
+use nannou::rand::{Rng, SeedableRng};
 
 struct Model {
     scheme_id: usize,
@@ -10,6 +11,8 @@ struct Model {
     gradient_one: Gradient<Hsl>,
     gradient_two: Gradient<Hsl>,
     gradient_three: Gradient<Hsl>,
+    blend_id: usize,
+    act_random_seed: u64,
 }
 
 fn main() {
@@ -19,11 +22,11 @@ fn main() {
 
 fn model(app: &App) -> Model {
     app.new_window()
-    //.size(720, 720)
-    .key_pressed(key_pressed)
-    .view(view)
-    .build()
-    .unwrap();
+        .key_pressed(key_pressed)
+        .mouse_pressed(mouse_pressed)
+        .view(view)
+        .build()
+        .unwrap();
 
     let scheme_id = 0;
     let palette = Palette::new();
@@ -44,9 +47,13 @@ fn model(app: &App) -> Model {
         gradient_one,
         gradient_two,
         gradient_three,
+        blend_id: 0,
+        act_random_seed: 0
     }
 }
-
+fn mouse_pressed(_app: &App, model: &mut Model, _button: MouseButton) {
+    model.act_random_seed = (random_f32() * 100000.0) as u64;
+}
 fn key_pressed(app: &App, model: &mut Model, key: Key) {
     match key {
         Key::Key1 => {
@@ -64,9 +71,26 @@ fn key_pressed(app: &App, model: &mut Model, key: Key) {
         Key::Key5 => {
             model.scheme_id = 4;
         }
+        Key::Key6 => {
+            model.scheme_id = 5;
+        }
+        Key::Q => {
+            model.blend_id = 0;
+        }
+        Key::W => {
+            model.blend_id = 1;
+        }
+        Key::E => {
+            model.blend_id = 2;
+        }
+        Key::R => {
+            model.blend_id = 3;
+        }
+
         Key::S => {
             app.main_window()
-                .capture_frame(app.exe_name().unwrap() + ".png");
+                    .capture_frame(app.time.to_string() + ".png");
+                //.capture_frame(app.exe_name().unwrap() + ".png");
         }
         _other_key => {}
     }
@@ -82,17 +106,20 @@ fn key_pressed(app: &App, model: &mut Model, key: Key) {
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
+    let mut rng = StdRng::seed_from_u64(model.act_random_seed);
     let win = app.window_rect();
-    println!("FPS:{:?}", app.fps());
-    let tile_count = map_range(app.mouse.x, win.w()*-1.0, win.w(), 1, 6) as u32;
+    let tile_count = map_range(app.mouse.x, win.w()*-1.0, win.w(), 1, 8) as u32;
 
-    let draw = app.draw();
-    let draw = draw.color_blend(BLEND_SUBTRACT);
-    //let draw = draw.color_blend(BLEND_ADD);
+    let blends = [
+        BLEND_NORMAL,
+        BLEND_ADD,
+        BLEND_SUBTRACT,
+        BLEND_LIGHTEST,
+    ];
+    let mut draw = app.draw().color_blend(blends[model.blend_id].clone());
     frame.clear(model.palette.get_scheme(model.scheme_id)[4]);
 
-    // TODO Work on single tile
-    // Add mouse interaction
+
     // Add gui to scale n tile
     // Add random
     let tot = tile_count * tile_count;
@@ -102,23 +129,30 @@ fn view(app: &App, model: &Model, frame: Frame) {
         let tile_h = win.h() / tile_count as f32;
         let x = (i % tile_count) as f32 * tile_w - win.w() * 0.5 + tile_w / 2.0;
         let y = (i / tile_count) as f32 * tile_h - win.h() * 0.5 + tile_h / 2.0;
-        draw_circle(&app, &draw, x, y, i, tile_w, tile_h, model);
-        draw_poly(&app, &draw, x, y, i, tile_w, tile_h, model);
+        let coin = rng.gen_range(0, 4);
+        draw_circles(&app, &draw, x, y, i, tile_w, tile_h, model, coin);
+        draw_poly(&app, &draw, x, y, i, tile_w, tile_h, model, coin);
+
+        let rotation = match coin {
+            0 => -(PI / 2.0),
+            1 => 0.0,
+            2 => PI / 2.0,
+            3 => PI,
+            _ => unreachable!(),
+        };
+        draw = draw.rotate(rotation);
     }
-
     draw.to_frame(app, &frame).unwrap();
-
-
 }
 
-fn draw_circle(app: &App, draw: &Draw, x: f32, y: f32, index: u32, _tile_w: f32, tile_h: f32, model: &Model) {
+fn draw_circles(app: &App, draw: &Draw, x: f32, y: f32, index: u32, tile_w: f32, tile_h: f32, model: &Model, random: u8) {
     let amp = 10.0;
     let wave = (app.time * 0.7).cos() * amp;
 
     draw.ellipse()
-        .color(model.gradient_one.get((app.time * 0.7).cos().abs()))
+        .color(model.gradient_one.get(0.5 + (app.time * 0.4).cos() * 0.5))
         .radius(tile_h / 2.0)
-        .x(x + wave * (index % 8) as f32)
+        .x(x + wave * (index % 3) as f32)
         .y(y);
     // shadow
     let n_circles = 8;
@@ -136,58 +170,50 @@ fn draw_circle(app: &App, draw: &Draw, x: f32, y: f32, index: u32, _tile_w: f32,
             .y(y)
             .radius(tile_h / 2.0 + offset - circle_thickness);
     }
+
+    let rad = tile_h/8.0 - app.mouse.y / 14.;
+    draw.ellipse()
+        .x(x-tile_w/2.0 + rad * 1.2)
+        .y((y+tile_h/2.0 - rad * 1.1) - wave * (index % 5) as f32)
+        .radius(rad)
+        .color(model.palette.get_scheme(model.scheme_id)[0])
+        .stroke_weight(3.)
+        .stroke_color(model.palette.get_scheme(model.scheme_id)[1]);
 }
 
-fn draw_poly(app: &App, draw: &Draw, x: f32, y: f32, _index: u32, tile_w: f32, tile_h: f32,  model: &Model) {
+fn draw_poly(app: &App, draw: &Draw, x: f32, y: f32, _index: u32, tile_w: f32, tile_h: f32,  model: &Model, random: u8) {
     let def = 9;
     let spaces = def -1;
     let parts = spaces + def;
-    let gambo_thick = tile_w / 9.0;
-    let v_padding = tile_h / parts as f32;
-    let t = app.time * 0.2;
+    let thickness = tile_w / 9.0;
     let tot = (parts*2)+2;
 
-    let points = (1..tot).map(|i| {
+    let points = (0..tot).map(|i| {
         let mut y2 = map_range(i, 1, tot, y-tile_h/2.0, y+tile_h/2.0);
-        let mut x2 = map_range(i, 1, tot, x-tile_w/2.0, x+tile_w/2.0);
+        let mut x2 = map_range(i, 1, tot, x-tile_w/2.0, x+tile_w/2.0 - thickness);
         let p = (i, tot);
         match p {
-            (i, total) if i == total => {
+            (i, total) if i == total-1 => {
                 x2 = x + tile_w / 2.0;
                 y2 = y + tile_h / 2.0;
             },
-            (1, _)  => {
+            (0, _)  => {
                 x2 = x + tile_w / 2.0;
                 y2 = y - tile_h / 2.0;
             },
-            (i, total) if (i % 4 == 0) | (i % 4 == 1) && i != total => {
-                x2 = x + tile_w/2.0 - gambo_thick;
+            (i, total) if (i % 4 == 0) | (i % 4 == 3) && i != total => {
+                x2 = x + tile_w/2.0 - thickness;
                 y2 = y2.floor();
             }
+            (i, total) if (i  == total) => {
+                x2 = x + tile_w / 2.0;
+                y2 = y - tile_h / 2.0;
+            },
             _ => {},
         };
-       // draw.ellipse().x(x-tile_w/2.0 + 20.).y(y+tile_h/2.0).radius(40.).color(RED);
-        //draw.ellipse().x(x2).y(y2).radius(10.).color(RED);
-        println!("x: {:?}, y: {:?}", x2, y2);
-        (pt2(x2, y2), model.gradient_two.get(p.0 as f32 / p.1 as f32))
+        let pick_gradient = map_range(y2, y-tile_h/2.0, y+tile_h/2.0, 0.0, 1.0);
+        let g = model.gradient_one.get(pick_gradient);
+        (pt2(x2, y2), g)
     });
-    println!("============tot: {:?}", tot);
-
-
-    // let points = (0..def).map(|i| {
-    //     let angle = map_range(i, 0, def, 0.0, TAU);
-    //
-    //     let off_x = angle.sin() * tile_w / 3.0;
-    //     let off_y = angle.cos() * tile_h / 3.0;
-    //     let fract = 0.5;
-    //
-    //     let r = (t + fract) % 1.0;
-    //     let g = ((t + 1.0 - fract) * angle) % 1.0;
-    //     let b = ((t + 0.5 + fract) * angle) % 1.0;
-    //
-    //     (pt2(x + off_x, y+off_y), palette[0])
-    // });
-
-    draw.ellipse().x(x+tile_w/2.0 +20.).y(y-tile_h/2.0).radius(40.).color(STEELBLUE);
     draw.polygon().points_colored(points);
 }
