42ab09b6be5c7552826448980607f4ee54115ad9
Davide Prati <lastexxit@gmail.com>
2020-06-30 23:55:38 +0200

    fix intersection

--- rayquad/src/old.rs
+++ rayquad/src/old.rs
@@ -23,11 +23,7 @@ impl Ray2D{
     }
 
     pub fn debug_ray(&self, draw: &Draw, mag: f32) {
-        //draw.arrow().weight(5.0).points(start, end);
-        draw.arrow()
-            .color(RED)
-            .start(self.orig)
-            .end(self.dir.with_magnitude(mag));
+        draw.line().color(RED).start(self.orig).end(self.dir.with_magnitude(mag));
     }
 
 
@@ -50,12 +46,20 @@ impl Ray2D{
             -((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3)) / den,
         );
 
+
         match tri {
             (d, t, u) if d != 0.0 && t > 0.0 && t < 1.0 && u > 0.0 =>
                 Some(vec2(x1 + t * (x2 - x1), y1 + t * (y2 - y1))),
             _ => None,
 
         }
+
+        // if (t > 0.0 && t < 1.0 && u > 0.0) {
+        //   Some(vec2(x1 + t * (x2 - x1), y1 + t * (y2 - y1)));
+        // } else {
+        //   None;
+        // }
+
     }
 }
 
@@ -67,7 +71,7 @@ struct Model {
     gradient_three: Gradient<Hsl>,
     blend_id: usize,
     act_random_seed: u64,
-    walls: Vec<f32>,
+    point: Vector2
 }
 
 fn model(app: &App) -> Model {
@@ -94,7 +98,7 @@ fn model(app: &App) -> Model {
         gradient_three,
         blend_id: 0,
         act_random_seed: 0,
-        walls: Vec::new(),
+        point: vec2(0.0,0.0),
     }
 }
 
@@ -151,15 +155,26 @@ fn mouse_pressed(_app: &App, model: &mut Model, _button: MouseButton) {
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
-    let draw = app.draw();
-    draw.background().color(PLUM);
-
     let start_line = vec2(300.0,100.0);
     let end_line = vec2(300.0,-300.0);
-
     let mut r = Ray2D::new();
     r.look_at(app.mouse.x, app.mouse.y);
 
+
+
+
+    let draw = app.draw();
+    let win = app.window_rect();
+    let tile_count_w = map_range(app.mouse.x, win.w()*-1.0, win.w(), 1, 8) as u32;
+    let tile_count_h = (win.h() * 2.0).abs() as u32 / tile_count_w;
+    let mut rng = StdRng::seed_from_u64(model.act_random_seed);
+
+    let tot = tile_count_w * tile_count_h;
+
+
+    draw.background().color(PLUM);
+
+    draw.ellipse().color(STEELBLUE).x_y(app.mouse.x, app.mouse.y);
     r.debug_ray(&draw, 200.0);
     draw.line().color(STEELBLUE).start(start_line).end(end_line);
 
