7dd79caff5d6517e0544d87474d08be59c6e804e
Davide Prati <lastexxit@gmail.com>
2022-01-28 10:38:33 +0100

    WIP post processing

--- screeneffect/src/main.rs
+++ screeneffect/src/main.rs
@@ -2,6 +2,8 @@
 // as a PNG and displaying a down-scaled version of the image within the window each frame.
 
 use nannou::prelude::*;
+pub mod post_processing_effect;
+pub use crate::post_processing_effect::PostProcessingEffect ;
 
 fn main() {
     nannou::app(model).update(update).run();
@@ -9,13 +11,7 @@ fn main() {
 
 struct Model {
     // The texture that we will draw to.
-    texture: wgpu::Texture,
-    // Create a `Draw` instance for drawing to our texture.
-    draw: nannou::Draw,
-    // The type used to render the `Draw` vertices to our texture.
-    renderer: nannou::draw::Renderer,
-    // The type used to resize our texture to the window texture.
-    texture_reshaper: wgpu::TextureReshaper,
+    effect: PostProcessingEffect
 }
 
 fn model(app: &App) -> Model {
@@ -28,86 +24,97 @@ fn model(app: &App) -> Model {
         .new_window()
         .size(win_w, win_h)
         .title("nannou")
+        .key_pressed(key_pressed)
         .view(view)
         .build()
         .unwrap();
     let window = app.window(w_id).unwrap();
 
     // Retrieve the wgpu device.
-    let device = window.swap_chain_device();
+    let device = window.device();
 
-    // Create our custom texture.
-    let sample_count = window.msaa_samples();
-    let texture = wgpu::TextureBuilder::new()
-        .size(texture_size)
-        // Our texture will be used as the OUTPUT_ATTACHMENT for our `Draw` render pass.
-        // It will also be SAMPLED by the `TextureResizer`.
-        .usage(wgpu::TextureUsage::OUTPUT_ATTACHMENT | wgpu::TextureUsage::SAMPLED)
-        // Use nannou's default multisampling sample count.
-        .sample_count(sample_count)
-        // Use a spacious 16-bit linear sRGBA format suitable for high quality drawing.
-        .format(wgpu::TextureFormat::Rgba16Float)
-        // Build it!
-        .build(device);
-
-    // Create our `Draw` instance and a renderer for it.
-    let draw = nannou::Draw::new();
-    let descriptor = texture.descriptor();
-    let renderer =
-        nannou::draw::RendererBuilder::new().build_from_texture_descriptor(device, descriptor);
-
-    // Create the texture reshaper.
-    let texture_view = texture.create_default_view();
-    let texture_component_type = texture.component_type();
-    let dst_format = Frame::TEXTURE_FORMAT;
-    let texture_reshaper = wgpu::TextureReshaper::new(
-        device,
-        &texture_view,
+    // set up the capturer
+    let sample_count = app.window(w_id).unwrap().msaa_samples();
+    let path = capture_directory(app);
+    let effect = PostProcessingEffect::new(
+        texture_size,
         sample_count,
-        texture_component_type,
-        sample_count,
-        dst_format,
+        app.window(w_id).unwrap().device(),
+        path,
+        false,
     );
+    // end capturer
+
+
 
     Model {
-        texture,
-        draw,
-        renderer,
-        texture_reshaper,
+        effect,
     }
 }
 
 fn update(app: &App, model: &mut Model, _update: Update) {
+    // Because we draw in the texture, all the code that usually goes in the view method has to be moved into the update
+    // function.
+
+    // VIEW
     // First, reset the `draw` state.
-    let draw = &model.draw;
+    let draw = &model.effect.draw;
     draw.reset();
+
 
     // Draw like we normally would in the `view`.
     draw.background().color(BLACK);
+
+
+    // we are not animating using time. But we should
+    // Use the frame number to animate, ensuring we get a constant update time.
+    let elapsed_frames = app.main_window().elapsed_frames();
+    let time = elapsed_frames as f32 / 60.0;
+    // let time = app.time;
     draw.ellipse()
-        .x_y(app.mouse.x, app.mouse.y)
+        .x_y(time.sin() * 200.0, time.cos()* 200.0)
         .w_h(200.0, 200.0)
         .color(RED);
 
     // Render our drawing to the texture.
     let window = app.main_window();
-    let device = window.swap_chain_device();
-    let ce_desc = wgpu::CommandEncoderDescriptor {
-        label: Some("texture renderer"),
-    };
-    let mut encoder = device.create_command_encoder(&ce_desc);
-    model
-        .renderer
-        .render_to_texture(device, &mut encoder, draw, &model.texture);
-    // Submit the commands for our drawing and texture capture to the GPU.
-    window.swap_chain_queue().submit(&[encoder.finish()]);
+    let device = window.device();
+    model.effect.update(&window, &device, elapsed_frames);
+
 }
 
 // Draw the state of your `Model` into the given `Frame` here.
 fn view(_app: &App, model: &Model, frame: Frame) {
     // Sample the texture and write it to the frame.
-    let mut encoder = frame.command_encoder();
-    model
-        .texture_reshaper
-        .encode_render_pass(frame.texture_view(), &mut *encoder);
+    model.effect.view(frame);
+}
+
+fn key_pressed(_app: &App, model: &mut Model, key: Key) {
+    match key {
+        Key::S => model.effect.take_screenshot(),
+        Key::R => model.effect.start_recording(),
+        Key::P => model.effect.stop_recording(),
+        // Key::S => match app.window(model.main_window_id) {
+        //     Some(window) => {
+        //         window.capture_frame(app.time.to_string() + ".png");
+        //     }
+        //     None => {}
+        // },
+        _other_key => {}
+    }
+}
+
+// The directory where we'll save the frames.
+fn capture_directory(app: &App) -> std::path::PathBuf {
+    app.project_path()
+        .expect("could not locate project_path")
+        .join(app.exe_name().unwrap())
+}
+
+fn exit(app: &App, model: Model) {
+    println!("Waiting for PNG writing to complete...");
+    let window = app.main_window();
+    let device = window.device();
+    model.effect.exit(&device);
+    println!("Done!");
 }
